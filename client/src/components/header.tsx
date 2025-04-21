dc9b67bdc6c32fcaaee0f6bba97808ef954a6a59
Maxence Agullo <magullo35@gmail.com>
2025-04-21 16:10:08 +0200

    added burger menu

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,11 +1,34 @@
 import "./header.css"
+import { useState } from "react";
 
 
-export const Header = () =>{
+
+
+export const Header = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleClick = () => {
+        setIsVisible(!isVisible)
+        console.log("click");
+    };
+
+
+
     return (
-        <nav className="HeaderNav">
-            <img src="./public/logo.png" alt="cow on a skateboard"/>
-            <h1>InterPark</h1>
-        </nav>
+        <>
+            <nav className="HeaderNav">
+                <img className="burger" src="./public/BButton.svg" onClick={handleClick}/>
+                <img src="./public/logo.png" alt="cow on a skateboard" />
+                <h1>InterPark</h1>
+            </nav>
+            <div className={isVisible? "burgerBarOn" : "burgerBarOff"}>
+                <img src="./public/cross.svg" onClick={handleClick}/>
+                <a>Home</a>
+                <a>SkateMap</a>
+                <a>TricksDex</a>
+                <a>Profil</a>
+                <a>Contact</a>
+            </div>
+        </>
     )
 }
