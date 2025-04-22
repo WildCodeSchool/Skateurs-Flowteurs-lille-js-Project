57b9bc6d0d61bcbe82d41d2d84378b13cb569c59
Jordan-182 <jordan.p62@outlook.fr>
2025-04-22 17:10:41 +0200

    added list of tricks and displayed it on the page

--- client/src/components/TricksContainer.tsx
+++ client/src/components/TricksContainer.tsx
@@ -1,4 +1,5 @@
 import styles from "./TricksContainer.module.css";
+import { tricksList } from "./TricksList";
 
 export const TricksContainer = () => {
   return (
@@ -14,6 +15,27 @@ export const TricksContainer = () => {
           Hard
         </button>
       </nav>
+      <ul className={styles.ul}>
+        {tricksList.map((trick) => (
+          <li key={trick.id} className={styles.trickCard}>
+            <iframe
+              width="260"
+              height="150"
+              src={trick.video}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              loading="lazy"
+              allowFullScreen
+            ></iframe>
+            <h2>{trick.name}</h2>
+            <p>Niveau : {trick.level}</p>
+            <p>Gain d'xp : {trick.xp}</p>
+            <button type="button">Valider</button>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
