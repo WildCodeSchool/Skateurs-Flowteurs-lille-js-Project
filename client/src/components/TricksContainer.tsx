e30c72fe59d1781ff556950d78a186bcdf628e73
Jordan-182 <152084774+Jordan-182@users.noreply.github.com>
2025-04-22 17:29:30 +0200

    Update TricksContainer.tsx
    added types

--- client/src/components/TricksContainer.tsx
+++ client/src/components/TricksContainer.tsx
@@ -1,6 +1,14 @@
 import styles from "./TricksContainer.module.css";
 import { tricksList } from "./TricksList";
 
+interface Trick {
+  id: number;
+  name: string;
+  video: string;
+  level: string;
+  xp: number;
+}
+
 export const TricksContainer = () => {
   return (
     <section className={styles.container}>
@@ -16,12 +24,12 @@ export const TricksContainer = () => {
         </button>
       </nav>
       <ul className={styles.ul}>
-        {tricksList.map((trick) => (
-          <li key={trick.id} className={styles.trickCard}>
+        {tricksList.map(({ name, id, video, xp, level }: Trick) => (
+          <li key={id} className={styles.trickCard}>
             <iframe
               width="260"
               height="150"
-              src={trick.video}
+              src={video}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -29,9 +37,9 @@ export const TricksContainer = () => {
               loading="lazy"
               allowFullScreen
             ></iframe>
-            <h2>{trick.name}</h2>
-            <p>Niveau : {trick.level}</p>
-            <p>Gain d'xp : {trick.xp}</p>
+            <h2>{name}</h2>
+            <p>Niveau : {level}</p>
+            <p>Gain d'xp : {xp}</p>
             <button type="button">Valider</button>
           </li>
         ))}
