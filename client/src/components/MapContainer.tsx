acc36d28344417596ac3c38a965ab839556693c5
ChrisG-WCS <godet.chris59@gmail.com>
2025-04-24 12:30:20 +0200

    fix MapContainer

--- client/src/components/MapContainer.tsx
+++ client/src/components/MapContainer.tsx
@@ -1,22 +1,22 @@
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import styles from './MapContainer.module.css';
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import styles from "./MapContainer.module.css";
 
 const MapContainer = () => {
-    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-    return (
-        <APIProvider apiKey={apiKey}>
-            <div className={styles.mapcontainer}>
-                <Map
-                    className={styles.map}
-                    defaultCenter={{ lat: 50.62925, lng: 3.057256 }} // Lille
-                    defaultZoom={10}
-                    gestureHandling={'greedy'}
-                    disableDefaultUI={true}
-                />
-            </div>
-        </APIProvider>
-    );
+  return (
+    <APIProvider apiKey={apiKey}>
+      <div className={styles.mapcontainer}>
+        <Map
+          className={styles.map}
+          defaultCenter={{ lat: 50.62925, lng: 3.057256 }} // Lille
+          defaultZoom={10}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        />
+      </div>
+    </APIProvider>
+  );
 };
 
 export default MapContainer;
--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -1,15 +1,13 @@
-import MapContainer from '../components/MapContainer';
-import styles from './Map.module.css';
+import MapContainer from "../components/MapContainer";
+import styles from "./Map.module.css";
 
 const MapPage = () => {
-    return (
-        <main className={styles.map}>
-            <h1 className={styles.title}>SkateMap</h1>
-            <div className={styles.mapWrapper}>
-                <MapContainer />
-            </div>
-        </main>
-    );
+  return (
+    <main className={styles.map}>
+      <h1 className={styles.title}>SkateMap</h1>
+      <MapContainer />
+    </main>
+  );
 };
 
 export default MapPage;
