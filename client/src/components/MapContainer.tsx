ae8f0165690fbeb097577a37f2cd6d0a36065482
ChrisG-WCS <godet.chris59@gmail.com>
2025-04-24 09:15:59 +0200

    Fix Zoom Map

--- client/src/components/MapContainer.tsx
+++ client/src/components/MapContainer.tsx
@@ -1,17 +1,17 @@
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 const MapContainer = () => {
-    const defaultCenter = { lat: 48.8566, lng: 2.3522 }; // Paris
     const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
     return (
         <APIProvider apiKey={apiKey}>
             <div style={{ width: '100%', height: '100vh' }}>
                 <Map
-                    center={defaultCenter}
-                    zoom={13}
-                    mapId="DEMO_MAP_ID"
-                    style={{ width: '100%', height: '100%' }}
+                    style={{ width: '100vw', height: '100vh' }}
+                    defaultCenter={{ lat: 48.8566, lng: 2.3522 }}
+                    defaultZoom={3}
+                    gestureHandling={'greedy'}
+                    disableDefaultUI={true}
                 />
             </div>
         </APIProvider>
