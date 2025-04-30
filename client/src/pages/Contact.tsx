41c6cdd978c25936519240cae44aec856f76d927
Salahedine Imoussaine <salah.imoussaine@gmail.com>
2025-04-30 15:17:57 +0200

    Refactor Contact component styles and structure; update class names and improve text formatting

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -2,30 +2,32 @@ import styles from "./Contact.module.css";
 
 export const Contact = () => {
   return (
-    <div className={styles.Contact}>
+    <div className={styles.contact}>
       <p>
-        Ce projet web a été réalisé dans le cadre d'un travail d'équipe composé
-        de quatre développeurs passionnés, dans un délai d'un mois.
-        <br />
-        <br />
-        Notre objectif : créer une application moderne en React et TypeScript,
-        avec un back-end, en mettant en œuvre de bonnes pratiques de
-        développement et une organisation Agile. <br />
-        <br />
-        Nous avons travaillé ensemble sur toutes les étapes du projet :
-        conception des maquettes sur Figma, modélisation des données,
-        développement des composants, intégration d'API, gestion des routes, et
-        mise en place d'un design responsive.
-        <br />
-        <br /> Ce site est le fruit d'un véritable travail collaboratif, où
-        chacun a pu apporter ses compétences et sa créativité.
+        <span>
+          {" "}
+          Ce projet web a été réalisé dans le cadre d'un travail d'équipe
+          composé de quatre développeurs passionnés, dans un délai d'un mois.
+        </span>
+
+        <span>
+          Notre objectif : créer une application moderne en React et TypeScript,
+          avec un back-end, en mettant en œuvre de bonnes pratiques de
+          développement et une organisation Agile.{" "}
+        </span>
+
+        <span>
+          Nous avons travaillé ensemble sur toutes les étapes du projet :
+          conception des maquettes sur Figma, modélisation des données,
+          développement des composants, intégration d'API, gestion des routes,
+          et mise en place d'un design responsive.
+        </span>
+
+        <span>
+          Ce site est le fruit d'un véritable travail collaboratif, où chacun a
+          pu apporter ses compétences et sa créativité.
+        </span>
       </p>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
 
       <div className={styles.profil}>
         <a href="https://github.com/Maxe-afk" target="_blank" rel="noreferrer">
