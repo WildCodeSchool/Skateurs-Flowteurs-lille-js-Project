e0c40bae207a443ee2c623c0fefb8d8e2d45785b
ChrisG-WCS <godet.chris59@gmail.com>
2025-04-30 11:24:49 +0200

    Fix MapPage & MapContainer

--- client/src/components/MapContainer.tsx
+++ client/src/components/MapContainer.tsx
@@ -1,21 +1,17 @@
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { Map } from "@vis.gl/react-google-maps";
 import styles from "./MapContainer.module.css";
 
 const MapContainer = () => {
-  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-
   return (
-    <APIProvider apiKey={apiKey}>
-      <div className={styles.mapcontainer}>
-        <Map
-          className={styles.map}
-          defaultCenter={{ lat: 50.62925, lng: 3.057256 }}
-          defaultZoom={10}
-          gestureHandling={"greedy"}
-          disableDefaultUI={true}
-        />
-      </div>
-    </APIProvider>
+    <div className={styles.mapcontainer}>
+      <Map
+        className={styles.map}
+        defaultCenter={{ lat: 50.62925, lng: 3.057256 }}
+        defaultZoom={10}
+        gestureHandling="greedy"
+        disableDefaultUI={true}
+      />
+    </div>
   );
 };
 
--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -1,12 +1,17 @@
+import { APIProvider } from "@vis.gl/react-google-maps";
 import MapContainer from "../components/MapContainer";
 import styles from "./Map.module.css";
 
 const MapPage = () => {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
   return (
-    <div className={styles.map}>
-      <h1 className={styles.title}>SkateMap</h1>
-      <MapContainer />
-    </div>
+    <APIProvider apiKey={apiKey}>
+      <div className={styles.map}>
+        <h1 className={styles.title}>SkateMap</h1>
+        <MapContainer />
+      </div>
+    </APIProvider>
   );
 };
 
