345dd80c7332bd0f8eed56564de355f8598efc5d
Jordan-182 <Jordan.P62@outlook.fr>
2025-05-02 18:53:32 +0200

    tricks list is now in the SQL database and can be fetched using an api route

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,10 +8,12 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
+import trickAction from "./modules/item/trickAction";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+router.get("/api/tricks", trickAction.browse);
 
 /* ************************************************************************* */
 
