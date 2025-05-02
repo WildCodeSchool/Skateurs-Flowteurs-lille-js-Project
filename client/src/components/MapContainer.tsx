9ae76c3628d5c0d30da891cf6b19c91047479768
ChrisG-WCS <godet.chris59@gmail.com>
2025-05-02 12:09:33 +0200

    Add fix add env.sample

--- client/src/components/MapContainer.tsx
+++ client/src/components/MapContainer.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useImperativeHandle } from "react";
+import { useImperativeHandle } from "react";
 import { Map, Marker, useMap } from "@vis.gl/react-google-maps";
 import styles from "./MapContainer.module.css";
 
@@ -8,35 +8,34 @@ export interface MapContainerRef {
 
 interface MapContainerProps {
   markerPosition?: { lat: number; lng: number };
+  ref?: React.Ref<MapContainerRef>;
 }
 
-const MapContainer = forwardRef<MapContainerRef, MapContainerProps>(
-  ({ markerPosition }, ref) => {
-    const map = useMap();
+const MapContainer = ({ markerPosition, ref }: MapContainerProps) => {
+  const map = useMap();
 
-    useImperativeHandle(ref, () => ({
-      panTo: (lat, lng) => {
-        if (map) {
-          map.panTo({ lat, lng });
-          map.setZoom(14);
-        }
-      },
-    }));
+  useImperativeHandle(ref, () => ({
+    panTo: (lat, lng) => {
+      if (map) {
+        map.panTo({ lat, lng });
+        map.setZoom(14);
+      }
+    },
+  }));
 
-    return (
-      <div className={styles.mapcontainer}>
-        <Map
-          className={styles.map}
-          defaultCenter={{ lat: 50.62925, lng: 3.057256 }}
-          defaultZoom={10}
-          gestureHandling="greedy"
-          disableDefaultUI={true}
-        >
-          {markerPosition && <Marker position={markerPosition} />}
-        </Map>
-      </div>
-    );
-  }
-);
+  return (
+    <div className={styles.mapcontainer}>
+      <Map
+        className={styles.map}
+        defaultCenter={{ lat: 50.62925, lng: 3.057256 }}
+        defaultZoom={10}
+        gestureHandling="greedy"
+        disableDefaultUI={true}
+      >
+        {markerPosition && <Marker position={markerPosition} />}
+      </Map>
+    </div>
+  );
+};
 
 export default MapContainer;
