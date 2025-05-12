ecfe1ba75e140bc73c06499988a05a2d7cd1c765
Maxence Agullo <magullo35@gmail.com>
2025-05-12 15:35:53 +0200

    added ternary to the iframe to remove errors in the console and added the sql db to the local host

--- client/src/components/TrickCard.tsx
+++ client/src/components/TrickCard.tsx
@@ -16,17 +16,19 @@ export const TrickCard = ({
   const { user } = useUser();
   return (
     <li key={trick.id} className={styles.trickCard}>
-      <iframe
-        width="260"
-        height="150"
-        src={trick.video}
-        frameBorder={0}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        loading="lazy"
-        allowFullScreen
-      ></iframe>
+      {trick.video ? (
+        <iframe
+          width="260"
+          height="150"
+          src={trick.video}
+          frameBorder={0}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          loading="lazy"
+          allowFullScreen
+        ></iframe>
+      ) : null}
       <h2>{trick.name}</h2>
       <p>Niveau : {trick.level}</p>
       <p>Gain d'xp : {trick.xp}</p>
